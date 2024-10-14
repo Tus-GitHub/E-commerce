b8ff4632075d4d9475b7d06f51d36e2b02d1da86
Tus-GitHub <tushardhull22@gmail.com>
2024-10-15 00:53:58 +0530

    connected mongodb database

---
+++ models/Product.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type:Number,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true, 
+    },
+});
+
+const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
+export default Product;
